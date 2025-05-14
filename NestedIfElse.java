30be1f880240afedb9989314a5d3055d08509d79
Thanushri R <thanu123.genai@gmail.com>
2025-05-14 22:23:34 +0530

    NestedIfElse.java

---
+++ NestedIfElse.java
@@ -0,0 +1,20 @@
+package com.tns.day1;
+
+public class Nestedifelse1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    int a=10,b=20,c=5
+		{
+    	if(a<b)
+		System.out.println("a is not greater");
+    	else;
+    	System.out.println("a is greater");
+		}
+    {
+    if(b<c)
+    	System.out.println("b is not greater");
+    else;
+    System.out.println("b");
+	}
+}
