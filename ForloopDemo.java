edea58d4b587a4c4f6b5fc93f9a41a7dc994c8cd
Thanushri R <thanu123.genai@gmail.com>
2025-05-14 22:18:51 +0530

    ForloopDemo.java

---
+++ ForloopDemo.java
@@ -0,0 +1,13 @@
+package com.tns.day1;
+
+public class ForloopDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     for(int i=1000;i>=1;i--)
+    	 System.out.println("the value of i is" +i);
+     
+//     System.out.println(i);
+	}
+
+}
