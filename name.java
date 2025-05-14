4686dc1d2825da175bf0baf77feafa5bdf5fde68
Thanushri R <thanu123.genai@gmail.com>
2025-05-14 22:22:19 +0530

    name.java

---
+++ name.java
@@ -0,0 +1,9 @@
+package com.tns.day1;
+public class name {
+public static void main(String[] args) {
+		int x=5;
+		if(x!=5)
+		  System.out.println("x is not equal to 5!");
+		System.out.println("x is equal to 5");
+}
+}
